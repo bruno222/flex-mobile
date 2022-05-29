279774dd5521d6d5cbf772dec618a8b6ef89b0be
Bruno Kilian <bkilian@twilio.com>
2022-05-29 23:59:27 +0200

    chore: loading in red

--- react-native/components/Loading.tsx
+++ react-native/components/Loading.tsx
@@ -1,11 +1,11 @@
-import { Box, Heading, HStack, Input, Spinner, Text } from 'native-base';
+import { Box, Heading, HStack, Spinner } from 'native-base';
 
 export const Loading = () => {
   return (
     <HStack space={2} justifyContent="center" alignItems="center" flex={1} backgroundColor="#e5ded4">
-      <Spinner accessibilityLabel="Loading messages" size="lg" />
-      <Heading color="primary.500" fontSize="xl">
-        Just a sec...
+      <Spinner accessibilityLabel="Loading messages" size="lg" color="red.400" />
+      <Heading color="red.400" fontSize="xl">
+        Just a second...
       </Heading>
     </HStack>
   );
