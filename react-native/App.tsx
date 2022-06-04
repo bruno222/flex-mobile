d28fed3fb5697aa96d15dcd166fa7474c0bd463b
Bruno Kilian <bkilian@twilio.com>
2022-06-04 04:22:34 +0200

    draft that works badly

--- react-native/App.tsx
+++ react-native/App.tsx
@@ -4,9 +4,16 @@ import { Main } from './screens/Main';
 import { RecoilRoot } from 'recoil';
 import { view } from '@risingstack/react-easy-state';
 import { tinyStore } from './store/tiny-store';
+import { useEffect } from 'react';
+import { registerForPushNotificationsAsync } from './helper/push-notification';
 
 function App() {
   const { token } = tinyStore;
+
+  // Push Notification
+  useEffect(() => {
+    registerForPushNotificationsAsync();
+  }, []);
 
   // Open browser with the Login URL in case token does not exist.
   if (!token) {
---
+++ react-native/helper/push-notification.tsx
@@ -0,0 +1,52 @@
+import * as Notifications from 'expo-notifications';
+import { Platform } from 'react-native';
+
+const _handleNotification = (notification: any) => {
+  console.log('@@Push - notification', notification);
+};
+
+const _handleNotificationResponse = (response: any) => {
+  console.log('@@Push - response', response);
+};
+
+let token = '';
+
+export const registerForPushNotificationsAsync = async () => {
+  if (Platform.OS !== 'android') {
+    console.log('Ignoring Push Notification, not an Android device...');
+    return;
+  }
+
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== 'granted') {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== 'granted') {
+    alert('Failed to get push token for push notification!');
+    return;
+  }
+
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log('@@Push - token', token);
+
+  // Only for Android:
+  Notifications.setNotificationChannelAsync('default', {
+    name: 'default',
+    importance: Notifications.AndroidImportance.MAX,
+    vibrationPattern: [0, 250, 250, 250],
+    lightColor: '#FF231F7C',
+  });
+
+  Notifications.setNotificationHandler({
+    handleNotification: async () => ({
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    }),
+  });
+
+  Notifications.addNotificationReceivedListener(_handleNotification);
+  Notifications.addNotificationResponseReceivedListener(_handleNotificationResponse);
+};
